ae942222e0f1995c6f35a9c72ebae7393487864d
Romario Williams <romariowilliams@hotmail.com>
2019-02-09 03:10:48 -0500

    Update header.js to use Link from react-router-dom

--- src/Components/headerComponent/header.js
+++ src/Components/headerComponent/header.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {
+  Link
+} from 'react-router-dom';
 
 class Header extends Component {
   render() {
@@ -11,13 +14,13 @@ class Header extends Component {
       <nav>
         <ul>
           <li className="first">
-              <a href="#">Home</a>
+              <Link to="/">Home</Link>
           </li>
           <li>
-              <a href="#">Products</a>
+              <Link to="/Products">Products</Link>
           </li>
           <li className="last">
-            <a href="#">Contacts</a>
+              <Link to="/Contact">Contact</Link>
           </li>
         </ul>
       </nav>
