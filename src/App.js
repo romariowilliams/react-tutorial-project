95e7e79c84b7d04ef79a1ba091b10741e496b5de
Romario Williams <romariowilliams@hotmail.com>
2019-02-09 03:09:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 //components
 import Header from './Components/headerComponent/header';
 import Footer from './Components/footerComponent/footer';
 import Home from './Components/pages/home';
+import Products from './Components/pages/products';
+import Contact from './Components/pages/contact';
 //includes
 import './Assets/css/default.min.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
       <Header/>
-        <Home/>
+
+      <Route exact path='/' component={Home} />
+      <Route exact path='/Products' component={Products} />
+      <Route exact path='/Contact' component={Contact} />
+
       <Footer/>
       </div>
-
+      </Router>
     );
   }
 }
