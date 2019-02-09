764413976676909f8a3fd2b61819a65ef287ac07
Romario Williams <romariowilliams@hotmail.com>
2019-02-09 03:09:35 -0500

    Updae products.js

---
+++ src/Components/pages/products.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+      Product Page Content Here
+
+      </div>
+    );
+  }
+}
+
+export default Products;
